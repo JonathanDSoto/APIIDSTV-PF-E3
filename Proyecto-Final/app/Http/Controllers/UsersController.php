8e22a35aee85d012d662b99d91631c97f8b58c93
jjacinto17 <jjacinto_20@alu.uabcs.mx>
2023-11-28 14:07:15 -0700

    Lógica UsersController

--- Proyecto-Final/app/Http/Controllers/UsersController.php
+++ Proyecto-Final/app/Http/Controllers/UsersController.php
@@ -3,11 +3,73 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function users()
     {
-        return view('users');
+        //Recoge todos los elementos de la tabla users
+        $users = User::all();
+
+        //Los manda al front con el compact('users')
+        return view('users', compact('users'));
+    }
+
+    public function store(Request $request)
+    {
+        //Crea un nuevo usuario
+        $user = new User();
+
+         //insert con los nombres de los inputs del modal agregar
+        $user->name = $request->editName;
+        $user->last_name = $request->editLastName;
+        $user->email = $request->editEmail;
+        $user->password = bcrypt($request->editPassword); 
+        $user->role = $request->editRole;
+        $user->curp = $request->editCurp;
+        $user->hotels = $request->editHotels;
+
+        $user->save();
+
+        return redirect()->route('users');
+    }
+
+    //comunica id con el front
+    public function edit($id)
+    {
+        $user = User::find($id);
+
+        return view('users', compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        //Busca el usuario por ID
+        $user = User::find($id);
+
+        //Actualiza los dataos del usuario
+        $user->name = $request->input('name');
+        $user->last_name = $request->input('last_name');
+        $user->email = $request->input('email');
+        $user->role = $request->input('role');
+        $user->curp = $request->input('curp');
+        $user->hotels = $request->input('hotels');
+
+        //Guarda los registros
+        $user->save();
+
+        return redirect()->route('users');
+    }
+
+    public function destroy($id)
+    {
+        //Busca el usuario por ID
+        $user = User::find($id);
+
+        //Borra el usuario
+        $user->delete();
+
+        return redirect()->route('users');
     }
 }
--- Proyecto-Final/routes/web.php
+++ Proyecto-Final/routes/web.php
@@ -19,8 +19,18 @@ Route::get('/clients', [ClientsController::class, 'clients'])->name('clients');
 Route::get('/reservations', [ReservationsController::class, 'reservations'])->name('reservations');
 Route::get('/users', [UsersController::class, 'users'])->name('users');
 
+//Rutas de HotelsController
+
 Route::get('/hotels', [HotelsController::class, 'hotels'])->name('hotels');
 Route::post('/hotels', [HotelsController::class, 'store'])->name('hotels.store');
 Route::get('/hotels/{id}/edit', [HotelsController::class, 'store'])->name('hotels.edit');
 Route::put('/hotels/{hotel}', [HotelsController::class, 'update'])->name('hotels.update');
 Route::delete('/hotels/{hotel}', [HotelsController::class, 'destroy'])->name('hotels.destroy');
+
+//Rutas de UsersController
+
+Route::get('/users', [UsersController::class, 'users'])->name('users');
+Route::post('/users', [UsersController::class, 'store'])->name('users.store');
+Route::get('/users/{id}/edit', [UsersController::class, 'edit'])->name('users.edit');
+Route::put('/users/{id}', [UsersController::class, 'update'])->name('users.update');
+Route::delete('/users/{id}', [UsersController::class, 'destroy'])->name('users.destroy');
