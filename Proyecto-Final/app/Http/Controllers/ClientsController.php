25c54704c4c196f3c17438d6e96f9418b583432f
jjacinto17 <jjacinto_20@alu.uabcs.mx>
2023-12-02 14:17:39 -0700

    Logica ClientsController

--- Proyecto-Final/app/Http/Controllers/ClientsController.php
+++ Proyecto-Final/app/Http/Controllers/ClientsController.php
@@ -3,11 +3,80 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Client;
 
 class ClientsController extends Controller
 {
     public function clients()
     {
-        return view('clients');
+        $clients = Client::all();
+
+        return view('clients', compact('clients'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'addName' => 'required|string|regex:/^[a-zA-ZáéíóúÁÉÍÓÚ\s]+$/|max:255',
+            'addLastName' => 'required|string|regex:/^[a-zA-ZáéíóúÁÉÍÓÚ\s]+$/|max:255',
+            'addEmail' => 'required|email|unique:clients,email',
+            'addPhone' => 'required|digits:10',
+        ]);
+
+        //Crea un nuevo usuario
+        $clients = new Client();
+
+        //insert con los nombres de los inputs del modal agregar
+        $clients->name = $request->addName;
+        $clients->last_name = $request->addLastName;
+        $clients->email = $request->addEmail;
+        $clients->phone = $request->addPhone;
+
+        $clients->save();
+
+        return redirect()->route('clients');
+    }
+
+    //comunica id con el front
+    public function edit($id)
+    {
+        $clients = Client::find($id);
+
+        return view('clients', compact('clients'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'editName' => 'required|string|regex:/^[a-zA-ZáéíóúÁÉÍÓÚ\s]+$/|max:255',
+            'editLastName' => 'required|string|regex:/^[a-zA-ZáéíóúÁÉÍÓÚ\s]+$/|max:255',
+            'editEmail' => 'required|email|unique:clients,email,' . $id,
+            'editPhone' => 'required|digits:10',
+        ]);
+
+        //Busca el usuario por ID
+        $clients = Client::find($id);
+
+        //Actualiza los dataos del usuario
+        $clients->name = $request->editName;
+        $clients->last_name = $request->editLastName;
+        $clients->email = $request->editEmail;
+        $clients->phone = $request->editPhone;
+
+        //Guarda los registros
+        $clients->save();
+
+        return redirect()->route('clients');
+    }
+
+    public function destroy($id)
+    {
+        //Busca el usuario por ID
+        $clients = Client::find($id);
+
+        //Borra el usuario
+        $clients->delete();
+
+        return redirect()->route('clients');
     }
 }
