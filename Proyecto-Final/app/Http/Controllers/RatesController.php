e156ae0135cfd05c0cb552534fa0324ef0d20ece
jjacinto17 <jjacinto_20@alu.uabcs.mx>
2023-12-03 01:34:51 -0700

    Logica RatesController

--- Proyecto-Final/app/Http/Controllers/RatesController.php
+++ Proyecto-Final/app/Http/Controllers/RatesController.php
@@ -3,11 +3,64 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Rates;
 
 class RatesController extends Controller
 {
     public function rates()
     {
-        return view('rates');
+        $rates = Rates::all();
+
+        return view('rates', compact('rates'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'addNameRate' => 'required|string|regex:/^[a-zA-ZáéíóúÁÉÍÓÚ\s]+$/|max:255',
+            'addPrice' => 'decimal:0,2',
+        ]);
+
+        $rates = new Rates();
+
+        $rates->name_rate = $request->addNameRate;
+        $rates->price = $request->addPrice;
+
+        $rates->save();
+
+        return redirect()->route('rates');
+    }
+
+    public function edit($id)
+    {
+        $rates = Rates::find($id);
+
+        return view('rates', compact('rates'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'addNameRate' => 'required|string|regex:/^[a-zA-ZáéíóúÁÉÍÓÚ\s]+$/|max:255',
+            'addPrice' => 'decimal:0,2',
+        ]);
+
+        $rates = Rates::find($id);
+
+        $rates->name_rate = $request->editNameRate;
+        $rates->price = $request->editPrice;
+
+        $rates->save();
+
+        return redirect()->route('rates');
+    }
+
+    public function destroy($id)
+    {
+        $rates = Rates::find($id);
+
+        $rates->delete();
+
+        return redirect()->route('rates');
     }
 }
