5cc38ad99d275afc97f348fa6e88bad407184778
jjacinto17 <jjacinto_20@alu.uabcs.mx>
2023-12-06 16:32:56 -0700

    Datos para el Seeder

--- Proyecto-Final/database/seeders/DatosPruebaSeeder.php
+++ Proyecto-Final/database/seeders/DatosPruebaSeeder.php
@@ -6,23 +6,23 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Hotel;
 use App\Models\User;
+use App\Models\Rates;
+use App\Models\Room;
 
-class DatosPruebaSeeder extends Seeder
-{
-    public function run()
-    {
+class DatosPruebaSeeder extends Seeder {
+    public function run() {
         Hotel::create([
             'name' => 'Hotel A',
             'address' => 'Dirección del Hotel A',
             'description' => 'Descripción del Hotel A',
-            'image'=> 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.visitorlando.com%2Falojamiento%2Ftodos-los-hoteles-y-resorts%2F&psig=AOvVaw1w2-V6dlomhgeDK-n8KqNN&ust=1701386697453000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCJD1icGt6oIDFQAAAAAdAAAAABAD',
+            'image' => 'https://www.momondo.mx/himg/62/c0/84/ice-85676218-68620422_3XL-430714.jpg',
         ]);
 
         Hotel::create([
             'name' => 'Hotel B',
             'address' => 'Dirección del Hotel B',
             'description' => 'Descripción del Hotel B',
-            'image'=> 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.visitorlando.com%2Falojamiento%2Ftodos-los-hoteles-y-resorts%2F&psig=AOvVaw1w2-V6dlomhgeDK-n8KqNN&ust=1701386697453000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCJD1icGt6oIDFQAAAAAdAAAAABAD',
+            'image' => 'https://media-cdn.tripadvisor.com/media/photo-s/16/1a/ea/54/hotel-presidente-4s.jpg',
         ]);
 
         User::create([
@@ -30,7 +30,7 @@ class DatosPruebaSeeder extends Seeder
             'last_name' => 'Meza',
             'email' => 'correo@prueba.com',
             'password' => bcrypt('123456789'),
-            'name_hotel'=> 'Hotel A'
+            'name_hotel' => 'Hotel A'
         ]);
 
         User::create([
@@ -38,7 +38,51 @@ class DatosPruebaSeeder extends Seeder
             'last_name' => 'Jacinto',
             'email' => 'correo2@prueba.com',
             'password' => bcrypt('123456789'),
-            'name_hotel'=> 'Hotel B'
+            'name_hotel' => 'Hotel B'
+        ]);
+
+        Rates::create([
+            'name_rate' => 'Habitación Sencilla',
+            'price' => '399.99',
+        ]);
+
+        Rates::create([
+            'name_rate' => 'Habitación Deluxe',
+            'price' => '899.99',
+        ]);
+
+        Rates::create([
+            'name_rate' => 'Habitación Premium',
+            'price' => '1499.99',
+        ]);
+
+        Room::create([
+            'image'=> 'https://images.hola.com/imagenes/decoracion/20230425230358/dormitorios-inspirados-en-habitaciones-hoteles-am/1-237-28/habitaciones-hotel-5a-a.jpg',
+            'name_room'=> 'Habitación Sencilla',
+            'description'=> '1 Cama',
+            'state'=> 'Libre',
+            'hotel_name'=> 'Hotel A',
+            'rate_room'=> '399.99',
+        ]);
+
+        Room::create([
+            'image'=> 'https://blog.lobbyroomhotel.com/wp-content/uploads/2019/02/unnamed-1.jpg',
+            'name_room'=> 'Habitación Deluxe',
+            'description'=> '1 Cama Familiar',
+            'state'=> 'Libre',
+            'hotel_name'=> 'Hotel A',
+            'rate_room'=> '899.99',
+        ]);
+
+        Room::create([
+            'image'=> 'https://blacktowerhotel.com/wp-content/uploads/2019/03/habitacion_deluxe.jpeg',
+            'name_room'=> 'Habitación Premium',
+            'description'=> '1 Cama Familiar',
+            'state'=> 'Libre',
+            'hotel_name'=> 'Hotel A',
+            'rate_room'=> '1499.99',
         ]);
     }
 }
+
+
