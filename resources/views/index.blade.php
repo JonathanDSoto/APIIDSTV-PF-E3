83f551c8b35272c92dba6c8bcb2b8224bd957336
angeluxwinged <angeluxwinged@gmail.com>
2023-12-08 18:22:40 -0700

    muestra hotel seleccionado en el index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,14 +12,14 @@
                         </div>
                         <div class="dropdown relative">
                             <button class="btn inline-flex justify-center btn-outline-light items-center cursor-default relative !pr-14"
-                                    type="button" id="lightsplitOutlineDropdownMenuButton" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                Mostrar por Hotel
-                                <span class="cursor-pointer absolute ltr:border-l rtl:border-r border-light-500 h-full ltr:right-0 rtl:left-0 px-2 flex
-                                              items-center justify-center leading-none">
-                                    <iconify-icon class="leading-none text-xl" icon="ic:round-keyboard-arrow-down"></iconify-icon>
-                                </span>
+                            type="button" id="lightsplitOutlineDropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span id="selectedHotel">{{ session('user_name_hotel') }}</span>
+                            <span class="cursor-pointer absolute ltr:border-l rtl:border-r border-light-500 h-full ltr:right-0 rtl:left-0 px-2 flex
+                                items-center justify-center leading-none">
+                                <iconify-icon class="leading-none text-xl" icon="ic:round-keyboard-arrow-down"></iconify-icon>
+                            </span>
                             </button>
+
                             <ul class="dropdown-menu min-w-max absolute text-sm text-slate-700 dark:text-white hidden bg-white dark:bg-slate-700 shadow
                                       z-[2] float-left overflow-hidden list-none text-left rounded-lg mt-1 m-0 bg-clip-padding border-none">
                                 @foreach ($hotels as $hotel)
@@ -44,8 +44,9 @@
                                         @method('PUT')
                                         <input type="hidden" name="id" value="{{ $room->id }}">
 
-                                    <img src="{{ $room->image }}"
+                                    <img style="width: 200px; height: 120px" src="{{ $room->image }}"
                                          class="mx-auto mb-4 rounded-full">
+                                         
                                      <div class="max-w-[160px] mx-auto mt-6">
                                          <div class="widget-title">{{ $room->name_room }}</div>
                                      </div>
@@ -73,17 +74,19 @@
                     
                         <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
                         <script>
+                             var selectedHotel = '{{ session('user_name_hotel') }}';
                             $(document).ready(function() {
-                                var defaultHotel = '{{ session('user_name_hotel') }}';
-                                filterRooms(defaultHotel);
+                                filterRooms(selectedHotel);
                             });
                         
                             function filterRooms(selectedHotel) {
-                                $('.room-container').hide();
+                                $('#selectedHotel').text(selectedHotel);
                         
+                                $('.room-container').hide();
                                 $('.room-container[data-hotel="' + selectedHotel + '"]').show();
                             }
                         </script>
+                        
                     </div>
 
                 </div>
